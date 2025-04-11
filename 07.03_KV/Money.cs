4bea585b758854ede1862817220620c41e3fd163
kutail <kutep0v.vlad.m@gmail.com>
2025-04-11 23:09:54 +0500

    Добавьте файлы проекта.

---
+++ 07.03_KV/Money.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._03_KV
+{
+    public class Money
+    {
+        public double Sum { get; set; }
+        public Money(double sum)
+        {
+            Sum = sum;
+        }
+        public static implicit operator Money(double sum)
+        {
+            return new Money(sum);
+        }
+        public static Money operator + (Money a, Money b)
+        {
+            return new Money(a.Sum + b.Sum);
+        }
+        public static Money operator + (Money a, double b)
+        {
+            Money res = a + b;
+            return res;
+        }
+        public static Money operator - (Money a, Money b)
+        {
+            return new Money(a.Sum - b.Sum);
+        }
+        public static Money operator - (Money a, double b)
+        {
+            Money res = a - b;
+            return res;
+        }
+        public static Money operator * (Money a, int b)
+        {
+            return new Money(a.Sum * b);
+        }
+        public static Money operator / (Money a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Нельзя делить на 0");
+            }
+            return new Money(a.Sum / b);
+        }
+        public static Money operator ++ (Money num)
+        {
+            return new Money(num.Sum + 0.1);
+        }
+        public static Money operator -- (Money num)
+        {
+            return new Money(num.Sum - 0.1);
+        }
+        public static bool operator > (Money a, Money b)
+        {
+            return a.Sum > b.Sum;
+        }
+        public static bool operator < (Money a, Money b)
+        {
+            return a.Sum < b.Sum;
+        }
+        public static bool operator == (Money a, Money b)
+        {
+            return a.Sum == b.Sum;
+        }
+        public static bool operator != (Money a, Money b)
+        {
+            return a.Sum != b.Sum;
+        }
+        public bool IsNegative()
+        {
+            return Sum < 0;
+        }
+    }
+}
---
+++ 07.03_KV/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _07._03_KV
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Money a = new Money(10.52);
+            Money b = new Money(5.1);
+
+            Money addResult = a + b;
+            Console.WriteLine($"Сложение: {a} + {b} = {addResult}");
+            Money subResult = a - b;
+            Console.WriteLine($"Вычитание: {a} - {b} = {subResult}");
+            Money mulResult = a * 3;
+            Console.WriteLine($"Умножение: {a} * 3 = {mulResult}");
+            Money divResult = a / 2;
+            Console.WriteLine($"Деление: {a} / 2 = {divResult}");
+            Money incResult = a++;
+            Console.WriteLine($"Инкремент: a++ = {a} (возвращает {incResult})");
+            Money decResult = b--;
+            Console.WriteLine($"Декремент: b-- = {b} (возвращает {decResult})");
+            Console.WriteLine($"Сравнение: {a} > {b} = {a > b}");
+            Console.WriteLine($"Сравнение: {a} < {b} = {a < b}");
+            Console.WriteLine($"Сравнение: {a} == {b} = {a == b}");
+            Console.WriteLine($"Сравнение: {a} != {b} = {a != b}");
+            Console.WriteLine($"Отрицательность: {a}.IsNegative() = {a.IsNegative()}");
+            Console.WriteLine($"Отрицательность: {subResult}.IsNegative() = {subResult.IsNegative()}");
+        }
+    }
+}
